b9674f33abe3d3de806c4b383b4d37a203f9f735
Rohit Patidar <rohitpatidar583@gmail.com>
2024-08-13 20:23:48 +0530

    create some section of the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection"
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcommingWebinars from "@/components/UpcommingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
  
       <HeroSection/>
+      <FeaturedCourses/>
+      <WhyChooseUs />
+      <MusicSchoolTestimonials/>
+      <UpcommingWebinars/>
     </main>
   );
 }
