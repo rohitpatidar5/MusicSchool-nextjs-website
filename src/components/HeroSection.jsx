fe75e7299b091d1c1f40778166f3bcfe2036a78f
Rohit Patidar <rohitpatidar583@gmail.com>
2024-08-13 18:01:47 +0530

    completed the hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
-
+import { Spotlight } from "./ui/Spotlight"
+import { Button } from "./ui/moving-border";
 
 function HeroSection() {
   return (
@@ -8,6 +9,10 @@ function HeroSection() {
     >
         
         <div className="p-4 relative z-10 w-full text-center" >
+        <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
             <h1
             className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400"
             >Master the art of music</h1>
@@ -16,8 +21,8 @@ function HeroSection() {
             >Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.</p>
             <div className="mt-4">
                 <Link href={"/courses"}>
-                    
-                    Explore courses
+                    <Button  >Explore courses
+                    </Button>
                     
                 </Link>
             </div>
